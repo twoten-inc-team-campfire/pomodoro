258522975f2bf20a603f44b0a2bd7f078bfde1c0
Ken Lin <xil307@ucsd.edu>
2020-11-03 11:35:10 -0800

    modify the initial app to a simple hellow world

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  return (
+    <div className="container">
+      <strong>Hello World</strong>
+    </div>
+  );
+};
+
+export default HelloWorld;
