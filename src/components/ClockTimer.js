e09172abb68fec9be4ebb116d202f7c109c14087
Chad Hayen <gigousfire@gmail.com>
2020-11-14 13:42:18 -0800

    Add PomodoroTimer and ClockTimer skeletons

---
+++ src/components/ClockTimer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {TimerInterface} from "../parts/PomodoroTimer";
+
+class ClockTimer extends TimerInterface {
+
+    constructor(props) {
+        super()
+    }
+
+    start() {}
+
+    pause() {}
+
+    reset() {}
+
+    render() {}
+}
+
+export default ClockTimer;
