743169c110dfcf7bb1913f00fc82407773ecabcc
Chad Hayen <gigousfire@gmail.com>
2020-11-17 18:30:07 -0800

    Fix JSDoc tags to output desired documentation

--- src/classes/settings/UserSettings.js
+++ src/classes/settings/UserSettings.js
@@ -2,17 +2,27 @@ import {PomodoroSettings} from "./PomodoroSettings";
 import {UISettings} from "./UISettings";
 
 /**
- * Contains user's current settings for the application. Container for other settings objects.
+ * UserSettings
+ * @desc Contains user's current settings for the application. Container for other settings objects.
  */
 export class UserSettings {
     /**
      * Create a UserSettings object.
+     * @constructs
      * @param {PomodoroSettings} pomodoro - The settings for the pomodoro timer
-     * @param {UISettings} ui - The settings for the
+     * @param {UISettings} ui - The settings for the UI
      */
     constructor(pomodoro= PomodoroSettings.defaults(),
                 ui= UISettings.defaults()) {
+        /**
+         * @member {PomodoroSettings} UserSettings#pomodoro
+         * @desc The settings for the pomodoro timer
+         */
         this.pomodoro = pomodoro
+        /**
+         * @member {UISettings} UserSettings#ui
+         * @desc The settings for the UI
+         */
         this.ui = ui
     }
 
--- src/components/ClockTimer.js
+++ src/components/ClockTimer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {TimerInterface} from "../parts/PomodoroManager";
 
+/**
+ * ClockTimer
+ * @desc A visual timer that displays the time remaining as text in a circular clockface.
+ * @implements {TimerInterface}
+ */
 class ClockTimer extends TimerInterface {
-
+    
     constructor(props) {
-        super()
+        super(props)
     }
 
     start() {}
