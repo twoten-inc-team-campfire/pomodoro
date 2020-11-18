6d6beb8cd2f3135c5ba7e1d3214799e3fd6b9f6f
Chad Hayen <gigousfire@gmail.com>
2020-11-17 19:51:10 -0800

    Fix bug in TimerSession constructor, remove redundant default functions in settings objects

--- src/classes/settings/UISettings.js
+++ src/classes/settings/UISettings.js
@@ -12,8 +12,10 @@ export class UISettings {
      * @param {boolean} displayFastForwardButton - Whether to display the fast forward button on the home screen.
      * @param {boolean} displayTaskSelector - Whether to display the task selector field on the home screen.
      */
-    constructor(displayPauseButton = true, displayCancelButton = true,
-                displayFastForwardButton = true, displayTaskSelector = true) {
+    constructor(displayPauseButton = true,
+                displayCancelButton = true,
+                displayFastForwardButton = true,
+                displayTaskSelector = true) {
         /**
          * @member {boolean} UISettings#displayPauseButton
          * @desc Whether to display the pause button on the home screen.
@@ -34,18 +36,5 @@ export class UISettings {
          * @desc Whether to display the task selector field on the home screen.
          */
         this.displayTaskSelector = displayTaskSelector
-    }
-
-    /**
-     * Returns a UISettings object with default settings.
-     * @returns {UISettings}
-     */
-    static defaults() {
-        return new UISettings(
-            true, // Display the pause button by default
-            true, // Display the cancel button by default
-            true, // Display the fast forward button by default
-            true // Display the task selector button by default
-        )
     }
 }
