75d86a639f1320425a5738e653b3b89d15df804c
Andrew Whitaker <a1whitak@eng.ucsd.edu>
2020-11-18 16:02:38 -0800

    Timer that can count down from designated duration. Timer starts on mounting.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ClockTimer from './components/ClockTimer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ClockTimer minutes={5}/>
       </header>
     </div>
   );
