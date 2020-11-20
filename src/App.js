16d3a22eb9c15194115c4a6d0f6b3661f533d174
Ken Lin <xil307@ucsd.edu>
2020-11-19 18:03:24 -0800

    Ken: make the clock timer work

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ClockTimer from './components/ClockTimer';
-import ExampleManager from './components/ExampleManager';
+import TimerManager from './components/TimerManager';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ExampleManager 
-          timer={
-            <ClockTimer minutes={5} />
-          }
-        />
+        <TimerManager/>
       </header>
     </div>
   );
--- src/components/ClockTimer.js
+++ src/components/ClockTimer.js
@@ -6,87 +6,101 @@ import {TimerInterface} from "../parts/PomodoroManager";
  * @desc A visual timer that displays the time remaining as text in a circular clockface.
  * @implements {TimerInterface}
  */
-class ClockTimer extends TimerInterface {
 
-    constructor(props) {
-        super(props);
+ function Timer({min, sec}) {
+    const min_present = min < 10 ? '0' + min : min.toString()
+    const sec_present = sec < 10 ? '0' + sec : sec.toString()
+    const time_present = min_present + ':' + sec_present
+     return (
+        <div>
+            <h1>Third clock timer</h1>
+            <h2>{time_present}</h2>
+        </div>
+    )
+ }
+ 
+export default Timer;
+// class ClockTimer extends TimerInterface {
 
-        this.state = {
-            total_duration: props.minutes,
-            minutes: props.minutes,
-            seconds: 0
-        };
+//     constructor(props) {
+//         super(props);
 
-    }
+//         this.state = {
+//             total_duration: props.minutes,
+//             minutes: props.minutes,
+//             seconds: 0
+//         };
 
-    start() {
-        this.timerID = setInterval(
-            () => this.tick(),
-            1000
-        );
-    }
+//     }
 
-    pause() {
-        clearInterval(this.timerID);
-    }
+//     start() {
+//         this.timerID = setInterval(
+//             () => this.tick(),
+//             1000
+//         );
+//     }
 
-    reset() {
-        clearInterval(this.timerID);
-        this.setState({
-            minutes: this.state.total_duration,
-            seconds: 0
-        })
-    }
+//     pause() {
+//         clearInterval(this.timerID);
+//     }
 
-    render() {
-        return (
-            <div>
-                <h1>Third clock timer</h1>
-                <h2>{this.printTime()}</h2>
-            </div>
-        )
-    }
+//     reset() {
+//         clearInterval(this.timerID);
+//         this.setState({
+//             minutes: this.state.total_duration,
+//             seconds: 0
+//         })
+//     }
 
-    tick() {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Third clock timer</h1>
+//                 <h2>{this.printTime()}</h2>
+//             </div>
+//         )
+//     }
+
+//     tick() {
         
-        if (this.isFinished()) {
+//         if (this.isFinished()) {
 
-        }
-        else {
-            this.decrementTime();
-        }
+//         }
+//         else {
+//             this.decrementTime();
+//         }
         
-    }
+//     }
 
-    decrementTime() {
+//     decrementTime() {
 
-        this.setState(function(state, props) {
-            if (state.seconds === 0) {
-                return {
-                    seconds: 59,
-                    minutes: state.minutes - 1
-                }
-            } else {
-                return {
-                    seconds: state.seconds - 1
-                }
-            }
-        });
-    }
+//         this.setState(function(state, props) {
+//             if (state.seconds === 0) {
+//                 return {
+//                     seconds: 59,
+//                     minutes: state.minutes - 1
+//                 }
+//             } else {
+//                 return {
+//                     seconds: state.seconds - 1
+//                 }
+//             }
+//         });
+//     }
 
-    isFinished() {
-        return (this.state.seconds === 0 && this.state.minutes === 0);
-    }
+//     isFinished() {
+//         return (this.state.seconds === 0 && this.state.minutes === 0);
+//     }
 
-    printTime() {
-        var seconds_str = this.state.seconds.toString();
-        if (this.state.seconds < 10) {
-            seconds_str = '0' + seconds_str;
-        }
+//     printTime() {
+//         var seconds_str = this.state.seconds.toString();
+//         if (this.state.seconds < 10) {
+//             seconds_str = '0' + seconds_str;
+//         }
 
-        return this.state.minutes.toString() + ':' + seconds_str;
-    }
+//         return this.state.minutes.toString() + ':' + seconds_str;
+//     }
 
-}
+// }
 
-export default ClockTimer;
+// export default ClockTimer;
