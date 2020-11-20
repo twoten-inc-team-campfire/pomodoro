3c50050fdef3d73afcbdb16510a5b8a59c3004eb
Ken Lin <xil307@ucsd.edu>
2020-11-19 20:09:50 -0800

    rebase off main, and put things in nice order: App(Home(TimerManager(TimerClock & TimerButtons))) where A(B) means A has B or B is a child of A

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import ClockTimer from './components/ClockTimer';
-import ExampleManager from './components/ExampleManager';
 import Home from './pages/Home.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Home/>
       </header>
     </div>
       
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,12 @@
 import React, {Component} from 'react';
-import IconButton from '@material-ui/core/IconButton';
-import PlayIcon from '@material-ui/icons/PlayCircleFilled';
-import PauseIcon from '@material-ui/icons/PauseCircleFilled';
-import CancelIcon from '@material-ui/icons/Cancel';
-import FastForwardIcon from '@material-ui/icons/FastForward';
+import TimerManger  from '../components/TimerManager';
 
 
 /** Home description */
 function Home () {
-    return <div> Home </div>
+    return <div>  
+        <TimerManger/>
+    </div>
 }
 
-export default Home;
+export default Home
