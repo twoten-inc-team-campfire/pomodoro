64af40464011205290175d1e32ac159c109866f3
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2020-11-25 16:33:52 -0800

    Add task selector

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import TaskSelector from '../components/Timer/TaskSelector';
 import Timer from '../components/Timer/Timer';
 
 function Home () {
     return (
         <div>  
+            <TaskSelector/>
             <Timer/>
         </div>
     )
