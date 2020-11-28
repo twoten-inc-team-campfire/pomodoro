f32094d3db4f420283056efddb5ab53201aec83b
Chad Hayen <gigousfire@gmail.com>
2020-11-27 22:22:11 -0800

    Update Statistics page with new components, change incorrect CSS attribute in DateNavigator

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import DateNavigator from "../components/Statistics/DateNavigator";
+import TimelineGraph from "../components/Statistics/TimelineGraph";
+import DailyLog from "../components/Statistics/DailyLog";
 
 class Statistics extends Component {
     constructor() {
         super();
-        this.state = {date: new Date(), timerSessions:[]}
+        this.state = {date: new Date(), timerSessions: []}
     }
 
     updateDate(oldDate, newDate) {
@@ -14,9 +16,33 @@ class Statistics extends Component {
     render() {
         let updateDate = this.updateDate.bind(this)
         return (
-            <div className="Summary">
-                Statistics page for visualizing app usage.
-                <DateNavigator date={this.state.date} onDateChange={updateDate}/>
+            <div className="Summary" style={{
+                "display": "flex",
+                "flexDirection": "column",
+                "alignItems": "center"
+            }}>
+                Statistics Page
+                <div style={{
+                    "marginTop": "1em"
+                }}>
+                    <DateNavigator
+                        date={this.state.date}
+                        onDateChange={updateDate}
+                    />
+                </div>
+                <br/>
+                <div style={{
+                    "width": "max(min(80%, 50em), 40%)", "height": "auto", "marginTop": "1em"}}
+                >
+                    <TimelineGraph
+                        date={this.state.date}
+                        timerSessions={this.state.timerSessions}
+                    />
+                </div>
+                <br/>
+                <div style={{"width": "75%", "height": "auto", "marginTop": "1em"}}>
+                    <DailyLog date={this.state.date} timerSessions={this.state.timerSessions}/>
+                </div>
             </div>
         )
     }
