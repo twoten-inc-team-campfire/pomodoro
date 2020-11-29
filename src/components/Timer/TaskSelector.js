6d9686d077c3b0d7f331fa9cf5a8f84155760bab
gabbyevaristo <gabbyevaristo@Gabbys-Air.attlocal.net>
2020-11-29 15:25:06 -0800

    Added documentation to TaskSelector and TimerButtons

--- src/components/Timer/TaskSelector.js
+++ src/components/Timer/TaskSelector.js
@@ -6,35 +6,60 @@ import DialogContent from '@material-ui/core/DialogContent';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDownCircle';
-import CheckIcon from '@material-ui/icons/Check';
+import CreateIcon from '@material-ui/icons/Create';
 import CloseIcon from '@material-ui/icons/Close';
 
 
+/**
+ * TaskSelector
+ * @desc Component that allows users to enter a task for the current
+ * pomodoro session.
+ */
 function TaskSelector() {
     const [showSelector, setShowSelector] = useState(true);
     const [isDialogOpen, setIsDialogOpen] = useState(false);
     const [task, setTask] = useState(null);
-  
+
+    /**
+     * handleToggle
+     * @desc Hides the selector and opens the dialog.
+     */
     const handleToggle = () => {
         setShowSelector(!showSelector)
         setIsDialogOpen(!isDialogOpen)
     }
 
+    /**
+     * handleChange
+     * @desc Sets the task to the input provided by the user.
+     */
     const handleChange = (event) => {
         setTask(event.target.value)
     }
 
+    /**
+     * handleSubmit
+     * @desc Closes the dialog.
+     */
     const handleSubmit = () => {
         setIsDialogOpen(!isDialogOpen)
     }
 
+    /**
+     * handleCancel
+     * @desc Shows the selector, closes the dialog, and sets the task 
+     * to null.
+     */
     const handleCancel = () => {
-        handleToggle();
+        handleToggle()
         setTask(null)
     }
 
-    const handleTaskClear = () => {
+    /**
+     * handleClearTask
+     * @desc Shows the selector and sets the task to null.
+     */
+    const handleClearTask = () => {
         setShowSelector(!showSelector)
         setTask(null)
     }
@@ -42,26 +67,21 @@ function TaskSelector() {
     return ( 
         <Fragment>
             <div className="task-box">
-            { showSelector &&
-                <IconButton aria-label="selector-button" onClick={handleToggle} >
-                    <ArrowDropDownIcon style={{ fontSize: '35px', color: '#015384' }} />
-                </IconButton>
-            }
+                { showSelector &&
+                    <IconButton aria-label="selector-button" onClick={handleToggle} >
+                        <CreateIcon style={{ fontSize: '35px', color: '#015384' }} />
+                    </IconButton>
+                }
 
             { task || 'No Task Selected' }
 
             <br></br>
 
-            { !isDialogOpen && task &&
-                <span>
-                    <IconButton aria-label="complete-button" onClick={handleTaskClear} >
-                        <CheckIcon style={{ fontSize: '35px', color: '#015384' }} />
-                    </IconButton>
-                    <IconButton aria-label="cancel-button" onClick={handleTaskClear} >
+                { !isDialogOpen && task &&
+                    <IconButton aria-label="cancel-button" onClick={handleClearTask} >
                         <CloseIcon style={{ fontSize: '35px', color: '#015384' }} />
                     </IconButton>
-                </span>
-            }
+                }
 
             <Dialog open={isDialogOpen} onClose={handleCancel} >
                 <DialogContent>
