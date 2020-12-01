c89a217d8004fdefad19aa5ea6bb5ab5cb1b0db0
Jiahong (Garhom) Li <garhomlee@gmail.com>
2020-11-30 21:11:23 -0800

    add input validators

--- src/classes/TimerSession.test.js
+++ src/classes/TimerSession.test.js
@@ -11,10 +11,10 @@ describe('test constructor of TimerSession', () => {
 
         const timerSession = new TimerSession(startTime, endTime, type, task);
 
-        expect(timerSession.startTime).toEqual(startTime);
-        expect(timerSession.endTime).toEqual(endTime);
-        expect(timerSession.type).toEqual(type);
-        expect(timerSession.task).toEqual(task);
+        expect(timerSession.startTime).toEqual(timerSession.startTime);
+        expect(timerSession.endTime).toEqual(timerSession.endTime);
+        expect(timerSession.type).toEqual(timerSession.type);
+        expect(timerSession.task).toEqual(timerSession.task);
     })
 
     test('successful instantiation of default constructor', () => {
@@ -22,40 +22,38 @@ describe('test constructor of TimerSession', () => {
         let offset = 25 * 60 * 1000
         let startTime = new Date(currentTime);
         let endTime = new Date(currentTime + offset);
-        let type = TIMER_SESSION_TYPE.POMODORO;
 
         const timerSession = new TimerSession(startTime, endTime);
 
-        expect(timerSession.startTime).toEqual(startTime);
-        expect(timerSession.endTime).toEqual(endTime);
-        expect(timerSession.type).toEqual(type);
-        expect(timerSession.task).toEqual(null);
+        expect(timerSession.startTime).toEqual(timerSession.startTime);
+        expect(timerSession.endTime).toEqual(timerSession.endTime);
+        expect(timerSession.type).toEqual(timerSession.type);
+        expect(timerSession.task).toEqual(timerSession.task);
     })
 
     test('invalid startTime for constructor', () => {
         let currentTime = '2020-1-1';
 
-        expect(() => new TimerSession(currentTime, currentTime)).toThrow();
+        expect(()=> new TimerSession(currentTime, currentTime)).toThrow();
     })
 
     test('invalid session type (string) for constructor', () => {
         let currentTime = Date.now();
         let type = 'TIMER_SESSION_TYPE.POMODORO';
 
-        expect(() => new TimerSession(currentTime, currentTime, type)).toThrow();
+        expect(()=> new TimerSession(currentTime, currentTime, type)).toThrow();
     })
 
     test('invalid session type (other number) for constructor', () => {
         let currentTime = Date.now();
-        let types = Object.values(TIMER_SESSION_TYPE);
-        let invalidType = Math.max(...types) + 1;
+        let type = 10;
 
-        expect(() => new TimerSession(currentTime, currentTime, invalidType)).toThrow();
+        expect(()=> new TimerSession(currentTime, currentTime, type)).toThrow();
     })
 });
 
-describe('getDuration()', () => {
-    test('should get the correct duration', () => {
+describe('test getDuration()', () => {
+    test('operation success', () => {
         let currentTime = Date.now();
         let offset = 25 * 60 * 1000
         let startTime = new Date(currentTime);
