8cd1eb2233223de8a78d663a05ddf25dea04044f
Andrew Whitaker <a1whitak@eng.ucsd.edu>
2020-12-02 12:31:11 -0800

    Established tests for Settings Selectors.

--- src/pages/Settings.test.js
+++ src/pages/Settings.test.js
@@ -4,7 +4,7 @@ import userEvent from '@testing-library/user-event'
 import Settings from './Settings'
 import { renderHelper } from '../utils/TestHelper'
 
-const customSettings = {
+const initialSettings = {
     settings: {
         autoBreak: true,
         autoFocus: false,
@@ -20,7 +20,7 @@ const customSettings = {
 }
 
 function basicSetup() {
-    renderHelper(<Settings/>, customSettings);
+    renderHelper(<Settings/>, initialSettings);
 }
 
 test ("Setting Headers are Visible", () => {
@@ -36,65 +36,141 @@ describe("Setting Switches and CheckBoxes", () => {
     test("Auto Break Switch Functions Properly", () => {
         basicSetup();
         // Initial Condition
-        var autoBreakSwitch = screen.getByRole('checkbox', {name: /auto break checkbox/i});
-        expect(autoBreakSwitch.checked).toEqual(customSettings.settings.autoBreak);
+        const autoBreakSwitch = screen.getByRole('checkbox', {name: /auto break checkbox/i});
+        expect(autoBreakSwitch.checked).toEqual(initialSettings.settings.autoBreak);
 
         // Change after user event
         userEvent.click(autoBreakSwitch);
-        expect(autoBreakSwitch.checked).not.toEqual(customSettings.settings.autoBreak);
+        expect(autoBreakSwitch.checked).not.toEqual(initialSettings.settings.autoBreak);
     })
 
     test("Auto Focus Switch Functions Properly", () => {
         basicSetup();
 
         // Initial Condition
-        var autoFocusSwitch = screen.getByRole('checkbox', {name: /auto focus checkbox/i});
-        expect(autoFocusSwitch.checked).toEqual(customSettings.settings.autoFocus);
+        const autoFocusSwitch = screen.getByRole('checkbox', {name: /auto focus checkbox/i});
+        expect(autoFocusSwitch.checked).toEqual(initialSettings.settings.autoFocus);
 
         // Change after user event
         userEvent.click(autoFocusSwitch);
-        expect(autoFocusSwitch.checked).not.toEqual(customSettings.settings.autoFocus);
+        expect(autoFocusSwitch.checked).not.toEqual(initialSettings.settings.autoFocus);
     })
 
     test("Pause Checkbox functions correctly", () => {
         basicSetup();
 
-        var pauseButtonCheckbox = screen.getByRole('checkbox', {name: /pause button checkbox/i});
-        expect(pauseButtonCheckbox.checked).toEqual(customSettings.settings.pause);
+        const pauseButtonCheckbox = screen.getByRole('checkbox', {name: /pause button checkbox/i});
+        expect(pauseButtonCheckbox.checked).toEqual(initialSettings.settings.pause);
 
         userEvent.click(pauseButtonCheckbox);
-        expect(pauseButtonCheckbox.checked).not.toEqual(customSettings.settings.pause);
+        expect(pauseButtonCheckbox.checked).not.toEqual(initialSettings.settings.pause);
     })
 
     test("Cancel Checkbox functions correctly", () => {
         basicSetup();
 
-        var cancelButtonCheckbox = screen.getByRole('checkbox', {name: /cancel button checkbox/i});
-        expect(cancelButtonCheckbox.checked).toEqual(customSettings.settings.cancel);
+        const cancelButtonCheckbox = screen.getByRole('checkbox', {name: /cancel button checkbox/i});
+        expect(cancelButtonCheckbox.checked).toEqual(initialSettings.settings.cancel);
        
         userEvent.click(cancelButtonCheckbox);
-        expect(cancelButtonCheckbox.checked).not.toEqual(customSettings.settings.cancel);
+        expect(cancelButtonCheckbox.checked).not.toEqual(initialSettings.settings.cancel);
     })
 
     test("Fast Forward Checkbox functions correctly", () => {
         basicSetup();
 
-        var fastForwardCheckbox = screen.getByRole('checkbox', {name: /fast forward button checkbox/i});
-        expect(fastForwardCheckbox.checked).toEqual(customSettings.settings.fastForward);
+        const fastForwardCheckbox = screen.getByRole('checkbox', {name: /fast forward button checkbox/i});
+        expect(fastForwardCheckbox.checked).toEqual(initialSettings.settings.fastForward);
         
         userEvent.click(fastForwardCheckbox);
-        expect(fastForwardCheckbox.checked).not.toEqual(customSettings.settings.fastForward);
+        expect(fastForwardCheckbox.checked).not.toEqual(initialSettings.settings.fastForward);
     })
 
     test("Task Selector Checkbox functions correctly", () => {
         basicSetup();
 
-        var taskSelectorCheckbox = screen.getByRole('checkbox', {name: /task selector checkbox/i});
-        expect(taskSelectorCheckbox.checked).toEqual(customSettings.settings.taskSelection);
+        const taskSelectorCheckbox = screen.getByRole('checkbox', {name: /task selector checkbox/i});
+        expect(taskSelectorCheckbox.checked).toEqual(initialSettings.settings.taskSelection);
         
         userEvent.click(taskSelectorCheckbox);
-        expect(taskSelectorCheckbox.checked).not.toEqual(customSettings.settings.taskSelection);
+        expect(taskSelectorCheckbox.checked).not.toEqual(initialSettings.settings.taskSelection);
     })
+
+});
+
+describe("Value Selectors on Settings Page", () => {
+
+    test("Pomodoro Length Selector", () => {
+        basicSetup();
+
+        const selectButton = screen.getByRole('button', {name: /focus length selector/i})
+        expect(selectButton.innerHTML).toMatch(/25:/i);
+
+        // Open ListBox to display all the values
+        userEvent.click(selectButton);
+
+        // Find Option in the listbox that we want to select
+        const optionButton = screen.getByTestId("focusLength15");
+        userEvent.click(optionButton);
+
+        // Verify that the new value appears for the user.
+        expect(selectButton.innerHTML).toMatch(/15:/i);
+
+    });
+
+    test("Short Break Length Selector", () => {
+        basicSetup();
+
+        const selectButton = screen.getByRole('button', {name: /short break length selector/i})
+        expect(selectButton.innerHTML).toMatch(/5:/i);
+
+        // Open ListBox to display all the values
+        userEvent.click(selectButton);
+
+        // Find Option in the listbox that we want to select
+        const optionButton = screen.getByTestId("shortLength4");
+        userEvent.click(optionButton);
+
+        // Verify that the new value appears for the user.
+        expect(selectButton.innerHTML).toMatch(/4:/i);
+
+    });
+
+    test("Long Break Length Selector", () => {
+        basicSetup();
+
+        const selectButton = screen.getByRole('button', {name: /long break length selector/i})
+        expect(selectButton.innerHTML).toMatch(/20:/i);
+
+        // Open ListBox to display all the values
+        userEvent.click(selectButton);
+
+        // Find Option in the listbox that we want to select
+        const optionButton = screen.getByTestId("longLength25");
+        userEvent.click(optionButton);
+
+        // Verify that the new value appears for the user.
+        expect(selectButton.innerHTML).toMatch(/25:/i);
+
+    });
+
+    test("Pomodoro Count Selector", () => {
+        basicSetup();
+
+        const selectButton = screen.getByRole('button', {name: /pomodoro count selector/i})
+        expect(selectButton.innerHTML).toMatch(/4/i);
+
+        // Open ListBox to display all the values
+        userEvent.click(selectButton);
+
+        // Find Option in the listbox that we want to select
+        const optionButton = screen.getByTestId("pomocount1");
+        userEvent.click(optionButton);
+
+        // Verify that the new value appears for the user.
+        expect(selectButton.innerHTML).toMatch(/1/i);
+
+    });
 
 });
 
