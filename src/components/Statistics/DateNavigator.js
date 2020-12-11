354bc6ff29c10d31b60406c938eda2f46033839a
Chad Hayen <gigousfire@gmail.com>
2020-12-10 20:03:35 -0800

    Separate many of DateNavigator's date manipulation functions out into DateUtil, add tests for those functions

--- src/components/Statistics/DateNavigator.js
+++ src/components/Statistics/DateNavigator.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
 import IconButton from '@material-ui/core/IconButton';
+import {dateToHtmlDateString, htmlDateStringToDate, shiftDate} from "../../utils/DateUtil";
 
 
 /**
@@ -18,61 +19,22 @@ import IconButton from '@material-ui/core/IconButton';
  * @param {onDateChange} onDateChange - The
  */
 function DateNavigator({date, onDateChange}) {
-
-    /**
-     * changeDate
-     * @param {number} offset
-     * @returns {Date}
-     */
-    function changeDate(offset) {
-        let time = date.getTime()
-        time += (offset * 86400000)
-        return new Date(time)
-    }
-
-    /**
-     * displayDate
-     * @param {Date} date
-     * @returns {string}
-     */
-    function displayDate(date) {
-        let year = date.getFullYear().toString();
-        let month = (date.getMonth() + 1).toString(); // JS indexes months 0 - 11, so we need to increment. WHY!?
-        if (month.length < 2) // Unfortunately, HTML required that the length of the months be EXACTLY 2 digits.
-            month = "0" + month;
-        let day = date.getDate().toString(); // Likewise, the length of the days must be EXACTLY 2 digits.
-        if (day.length < 2)
-            day = "0" + day;
-        return `${year}-${month}-${day}`;
-    }
-
-    /**
-     * strToDate
-     * @param {string} str
-     * @returns {Date}
-     */
-    function strToDate(str) {
-        let substrs = str.split('-');
-        let newDate = new Date();
-        newDate.setFullYear(parseInt(substrs[0]), parseInt(substrs[1])-1, parseInt(substrs[2]));
-        return newDate;
-    }
     return (
         <div style={{"display": "flex", "alignItems": "center", "justifyContent": "center"}}>
             <IconButton
-                onClick={() => onDateChange(date, changeDate(-1))}
+                onClick={() => onDateChange(date, shiftDate(date,-1))}
             >
                 <ArrowLeftIcon/>
             </IconButton>
             <input type="date"
-                   value={displayDate(date)}
+                   value={dateToHtmlDateString(date)}
                    style={{border: "none"}}
-                   onInput={(ev) => onDateChange(date, strToDate(ev.target.value))}
-                   max={displayDate(new Date())}
+                   onInput={(ev) => onDateChange(date, htmlDateStringToDate(ev.target.value))}
+                   max={dateToHtmlDateString(new Date())}
             />
             <IconButton
-                onClick={() => onDateChange(date, changeDate(1))}
-                disabled={displayDate(date) === displayDate(new Date())}
+                onClick={() => onDateChange(date, shiftDate(date,1))}
+                disabled={dateToHtmlDateString(date) === dateToHtmlDateString(new Date())}
             >
                 <ArrowRightIcon/>
             </IconButton>
--- src/utils/DateUtil.test.js
+++ src/utils/DateUtil.test.js
@@ -1,4 +1,4 @@
-import {getDateMinutes, getDateHours} from "./DateUtil";
+import {getDateMinutes, getDateHours, dateToHtmlDateString, htmlDateStringToDate, shiftDate} from "./DateUtil";
 
 describe("getDateMinues should successfully output a date's minutes", () => {
     test("Test getDateMinutes to successfully output two digit minutes", () => {
@@ -46,4 +46,58 @@ describe("getDateMinues should successfully output a date's hours", () => {
         let testDateHours = getDateHours(testDate);
         expect(testDateHours).toEqual(hoursString);
     });
+})
+
+describe("shiftDate should be capable of arbitrarily shifting a date forward or backward by the offset", () => {
+    test("shiftDate should be able to shift a Date forward/backward by a day", () => {
+        // Happy leap year!
+        const oldDate = new Date(2024, 2, 28);
+        // 𝅘𝅥𝅮 One more day! 𝅘𝅥𝅮
+        const oneDayMore = shiftDate(oldDate, 1);
+        let correctDate = new Date(2024, 2, 29);
+        expect(oneDayMore).toEqual(correctDate);
+        // Wha...? That's not how the song goes.
+        const oneDayLess = shift(oldDate, -1);
+        correctDate = new Date(2024, 2, 27);
+        expect(oneDayLess).toEqual(correctDate);
+    })
+    test("shiftDate should be able to shift a Date forward/backward by a month", () => {
+        // Pfft, who even likes May? More like you may NOT.
+        const oldDate = new Date(2021, 4, 30);
+        // Okay, maybe I'm jumping a little more than one month.
+        const oneMonthMore = shiftDate(oldDate, 32);
+        // June? More like SWOON.
+        let correctDate = new Date(2021, 6, 1);
+        expect(oneMonthMore).toEqual(correctDate);
+        const oneMonthLess = shiftDate(oldDate, 31);
+        correctDate = new Date(2021, 3, 31);
+        expect(oneMonthLess).toEqual(correctDate);
+    })
+    test("shiftDate should be able to shift a Date forward/backward by a year", () => {
+        // See you ~NEXT YEAR~!
+        const oldDate = new Date(2022, 11, 31);
+        const oneYearMore = shiftDate(oldDate, 366);
+        let correctDate = new Date(2024, 1, 1);
+        expect(oneYearMore).toEqual(correctDate);
+        const oneYearLess = shiftDate(oldDate, 720);
+        // But when will I get a date?
+        correctDate = new Date(2020, 11, 31);
+        expect(oneYearLess).toEqual(correctDate);
+    })
+})
+
+test("dateToHtmlDateString should be able to convert a date to a string usable in an HTML element", () => {
+    // The doomed date itself before Y2k consumed us all.
+    const testDate = new Date(1999, 11, 31, 23, 59, 59, 999);
+    const str = dateToHtmlDateString(testDate);
+    const correctStr = "1999-12-31";
+    expect(str).toEqual(correctStr);
+})
+
+test("htmlDateStringToDate should be able to convert a date string from an HTML element to a Date object", () => {
+    // Merry Christmas!
+    const testStr = "2020-12-25";
+    const date = htmlDateStringToDate(testStr);
+    const correctDate = new Date(2020, 11, 25, 0, 0, 0, 0);
+    expect(date).toEqual(correctDate);
 })
