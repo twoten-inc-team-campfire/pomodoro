bd60f1d9e7b9c0fd7582457391d11da325b3eda7
Chad Hayen <gigousfire@gmail.com>
2020-12-13 20:51:28 -0800

    Add tests for DailyLog component

--- src/components/Statistics/DailyLog.js
+++ src/components/Statistics/DailyLog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {TimerSession} from "../../classes/TimerSession";
-import DailyLogSession from "./DailyLogSession";
+import {DailyLogSession} from "./DailyLogSession";
 
 /**
  * @typedef {TimerSession} DailyLog#TimerCycle
@@ -10,56 +10,59 @@ import DailyLogSession from "./DailyLogSession";
  * @property {number} pauseTime - The length of time the timer was paused during a part of the cycle in milliseconds.
  * @ignore
  */
+
+/**
+ * breakTimerSessionsIntoSeparateBlocks
+ * @desc Breaks an ordered sequence of TimerSessions into blocks of consecutive TimerSessions
+ * @param {TimerSession[]} timerSessions - The TimerSessions to be split up
+ * @param {number} maxGap - The maximum allowable gap in ms between two TimerSessions before they're split into separate blocks.
+ * @returns {[TimerSession[]]}
+ */
+export function breakTimerSessionsIntoSeparateBlocks(timerSessions, maxGap) {
+    let blocks = [];
+    if (timerSessions.length > 0) {
+        let currentBlock = [timerSessions[0]];
+        for (let i = 1; i < timerSessions.length; i += 1) {
+            if (timerSessions[i].startTime.getTime() -
+                currentBlock[currentBlock.length - 1].endTime.getTime() >= maxGap) {
+                blocks.push(currentBlock);
+                currentBlock = [timerSessions[i]];
+            } else {
+                currentBlock.push(timerSessions[i]);
+            }
+        }
+        blocks.push(currentBlock);
+    }
+    return blocks;
+}
+
 /**
  * DailyLog
  * @desc Displays a log of the TimerSessions for a specific day, showing information about groups of consecutive
  * TimerSessions: time started, total amount of work done, tasks that were worked on, etc.
- * @param {Date} date - The date whose information is being logged.
  * @param {TimerSessions[]} timerSessions - The TimerSessions recorded on the date passed in as an argument.
  * @param {number} gap - The permitted time gap between consecutive TimerSessions before they're split into separate
- * segments.
+ * segments (in minutes).
  * @returns {JSX.Element}
  * @constructor
  */
-export default function DailyLog({date, timerSessions, gap=20}) {
+export function DailyLog({timerSessions, gap=20}) {
     let items = []
     if (timerSessions.length > 0) {
         gap = gap * 60 * 1000;
         timerSessions = [...timerSessions];
-        let currentDay = new Date(date);
-        date.setHours(0, 0, 0, 0);
-        if (timerSessions[0].startTime < currentDay) {
-            let temp = timerSessions[0].copy();
-            temp.startTime = currentDay;
-            timerSessions[0] = temp;
-        }
-        let nextDay = new Date(date);
-        nextDay.setDate(nextDay.getDate() + 1);
-        if (timerSessions[timerSessions.length - 1].endTime > nextDay) {
-            let temp = timerSessions[timerSessions.length - 1];
-            temp.endTime = nextDay;
-            timerSessions[timerSessions.length - 1] = temp;
-        }
-        let sessions = [];
-        let currentSession = [timerSessions[0]];
-        for (let i = 1; i < timerSessions.length; i += 1) {
-            if (timerSessions[i].startTime.getTime() -
-                currentSession[currentSession.length - 1].endTime.getTime() >= gap) {
-                sessions.push(currentSession);
-                currentSession = [timerSessions[i]];
-            } else {
-                currentSession.push(timerSessions[i]);
-            }
-        }
-        sessions.push(currentSession);
-        items = sessions.map((session, index) => (
-            <div style={{"marginTop": "0.5em", "marginBottom": "0.5em"}}>
-                <DailyLogSession key={index} timerSessions={session}/>
+        let blocks = breakTimerSessionsIntoSeparateBlocks(timerSessions, gap);
+        items = blocks.map((session, index) => (
+            <div key={index} style={{"marginTop": "0.5em", "marginBottom": "0.5em"}}>
+                <DailyLogSession timerSessions={session}/>
             </div>
         ));
     }
     return (
-        <div style={{"display": "flex", "flexDirection": "column", "justifyContent": "center"}}>
+        <div
+            style={{"display": "flex", "flexDirection": "column", "justifyContent": "center"}}
+            aria-label={"Timer sessions log"}
+        >
             {items}
         </div>
     )
