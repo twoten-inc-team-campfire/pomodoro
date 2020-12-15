a043c32508b114e8d07f63a8bb988a2eb752dba4
Andrew Whitaker <a1whitak@eng.ucsd.edu>
2020-12-15 15:44:56 -0800

    Improved documentation for the settings reducers.

--- src/GlobalState/SettingReducerandActions.js
+++ src/GlobalState/SettingReducerandActions.js
@@ -1,6 +1,13 @@
 
 
-//Reducer takes an action and update the state: these are the actions.
+/**
+ * SettingsActions
+ * @desc Object whose properties are used as predefined functions for updating
+ * the settings portion of the Global Context. The target parameter in each 
+ * action is used by the GlobalStateReducer as well as the settingsReducer to
+ * funnel the action to the appropriate reducer. The other properties of each 
+ * action are the data that the reducer utilizes to perform the transformation.
+ */
 const SettingsActions = {
     CHANGE_SINGLE_SETTING: (name, value) => ({
         target: 'Single-Setting',
@@ -13,18 +20,17 @@ const SettingsActions = {
     }),
 };
 
-/* timer reducer to work with useReducer Hooks in react
- *
- * Arguments:
- *    @state: the current state of the app (like above, it contains the min and sec)
- *    @action: the "description" about what you want to do with the state
+/** 
+ * settingsReducer 
+ * @desc Within the Global Context State is the subcontext of Settings. This settings
+ * reducer is responsible for transforming the settings portion of global context.
+ * It takes the current state of settings and an action to transform the state.
  * 
- * What this function does:
- *        It looks into the "action", understand what the action want us to do with the state, 
- *        and implements the logic for that action
- *
- * Return Value:
- *.       return a new state. In this case, it's an object that contains "min" and "sec"
+ * @param {UserSettings} state - The current state of the settings (like above, it contains the min and sec)
+ * @param {SettingsActions} action - The action specifies how the state should be 
+ * transformed and provides the necessary data to do so.
+ * 
+ * @returns {UserSettings}
  */
 const settingsReducer = (state, action) => {
     switch (action.target) {
