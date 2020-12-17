3ed4fd4ea40bcb5d32df44009b5fcc2d9a700541
Chad Hayen <gigousfire@gmail.com>
2020-12-17 13:14:00 -0800

    Add setDateToMidnight function with documentation and tests

--- src/utils/ExampleUtil.js
+++ src/utils/ExampleUtil.js
@@ -1,2 +1,10 @@
-// Utility functions that will be reused throughout the project.
-// Example: date formatting, string conversion
+/**
+ * setDateToMidnight
+ * @desc Sets the date passed in to midnight local time.
+ * @param {Date} date - The date to be set to midnight
+ * @returns {Date}
+ */
+export function setDateToMidnight(date) {
+    date.setHours(0, 0, 0, 0);
+    return date;
+}
