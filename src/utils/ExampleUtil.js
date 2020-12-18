c4f0f2678a34ff0283e6845504c994ac062c7360
Chad Hayen <gigousfire@gmail.com>
2020-12-17 17:03:52 -0800

    Show example of malformed JSDocs comment

--- src/utils/ExampleUtil.js
+++ src/utils/ExampleUtil.js
@@ -1,10 +1,10 @@
 /**
  * setDateToMidnight
  * @desc Sets the date passed in to midnight local time.
- * @param {Date} date - The date to be set to midnight
+ * @param {[Date]} date - The date to be set to midnight
  * @returns {Date}
  */
 export function setDateToMidnight(date) {
-    date.setHours(0, 0, 0, 8);
+    date.setHours(0, 0, 0, 0);
     return date;
 }
