c51e4393016ab69f0d2cfc0db57eabade5ae00b8
Chad Hayen <gigousfire@gmail.com>
2020-12-17 16:36:25 -0800

    Show example of unused import

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -4,6 +4,7 @@ import {TimelineGraph} from "../components/Statistics/TimelineGraph";
 import {DailyLog} from "../components/Statistics/DailyLog";
 import {loadTimerSessionListByDate} from "../services/DefaultDataService";
 import {fitTimerSessionsIntoRange} from "../utils/StatisticsPageUtil";
+import {setDateToMidnight} from "../utils/ExampleUtil";
 
 export const statisticsLogMaxGap = 20;
 export const statisticsTimelineMaxGap = 10;
--- src/utils/ExampleUtil.test.js
+++ src/utils/ExampleUtil.test.js
@@ -3,7 +3,7 @@ import {setDateToMidnight} from "./ExampleUtil";
 describe("setDateToMidnight should set a date passed in to midnight", () => {
     test("It should set the date to the correct time, midnight", () => {
         let date = new Date(2020, 2, 3, 5, 6, 7, 8);
-        let correctDate = new Date(2020, 2, 3, 0, 0, 0, 1);
+        let correctDate = new Date(2020, 2, 3, 0, 0, 0, 0);
         date = setDateToMidnight(date);
         expect(date).toEqual(correctDate);
     })
