72badb77d8c201156402e4f0b81bcf0363417f6c
Chad Hayen <gigousfire@gmail.com>
2020-12-17 17:38:56 -0800

    Show successful pipeline

--- src/utils/ExampleUtil.js
+++ src/utils/ExampleUtil.js
@@ -1,7 +1,7 @@
 /**
  * setDateToMidnight
  * @desc Sets the date passed in to midnight local time.
- * @param {[Date]} date - The date to be set to midnight
+ * @param {Date} date - The date to be set to midnight
  * @returns {Date}
  */
 export function setDateToMidnight(date) {
